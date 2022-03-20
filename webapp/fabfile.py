ebf9b9f3ab3115d0e14db69f9bf0be90c42b4ea4
Daniel Houlihan <danielhoulihan@daniels-mbp.home>
2022-03-20 11:40:38 +0000

    Fabric and Gunicorn added for deployment

---
+++ webapp/fabfile.py
@@ -0,0 +1,19 @@
+from fabric import task
+
+@task
+def setup(c):
+    c.run('pip3 install -r requirements.txt')
+    c.run("python3 manage.py collectstatic --noinput")
+
+@task
+def run(c):
+    c.run("gunicorn webapp.wsgi:application --bind 0.0.0.0:8000 -D --pid pid.txt", replace_env=False, pty=False)
+
+@task
+def kill(c):
+    c.run("kill -9 `cat pid.txt`")
+
+@task
+def reset(c):
+    c.run("rm -r db.sqlite3")
+    c.run("python3 manage.py migrate")
--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -4,7 +4,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=)i*-g+md#_p$==q-8^q^5u^!7#)*(7r1$wh4$34t**^zk!aw$'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -76,3 +76,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
