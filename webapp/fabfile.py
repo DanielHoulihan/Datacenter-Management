f93b79a5b250e9b872edf467f089467e9a9b6d61
Daniel Houlihan <danielhoulihan@daniels-mbp.home>
2022-03-20 13:12:25 +0000

    startup.sh added

--- webapp/fabfile.py
+++ webapp/fabfile.py
@@ -2,9 +2,10 @@ from fabric import task
 
 @task
 def setup(c):
-    c.run('source env/bin/activate')
-    c.run('pip3 install -r requirements.txt')
-    c.run("python3 manage.py collectstatic --noinput")
+    c.run('virtualenv --no-site-packages --distribute .env && source .env/bin/activate && pip install -r requirements.txt', replace_env=True, pty=False)
+    # c.run('source env/bin/activate')
+    # c.run('pip3 install -r requirements.txt')
+    # c.run("python3 manage.py collectstatic --noinput")
 
 @task
 def run(c):
